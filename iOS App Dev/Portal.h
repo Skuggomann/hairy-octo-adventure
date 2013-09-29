b312c33575edbc8a1d1593433edfcaa393838487
Snorri Arnarson <User@Lions-Mac.local>
2013-09-29 16:02:49 +0000

    Added portal class and made sand read config file.
    Broke the project.

---
+++ iOS App Dev/Portal.h
@@ -0,0 +1,19 @@
+//
+//  Portal.h
+//  iOS App Dev
+//
+//  Created by Lion User on 29/09/2013.
+//  Copyright (c) 2013 Sveinn Fannar Kristjansson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Portal : CCPhysicsSprite {
+    ChipmunkSpace *_space;
+    
+}
+
+- (id)initWithSpace:(ChipmunkSpace *)space position:(CGPoint)position;
+
+@end
